c58fcfff0bf9e5ce245bc671b60c6bb227fd6de2
saildeep <jkoerner@odd-it.com>
2017-05-21 21:09:45 +0200

    add lythium support

--- build_maps.bat
+++ build_maps.bat
@@ -1,5 +1,5 @@
 @echo off
-SET maps=Tanoa Stratis Chernarus Takistan Australia
+SET maps=Tanoa Stratis Chernarus Takistan Australia lythium
 for %%a in (.) do set currentfolder=%%~na
 echo "%currentfolder%"
 (for %%a in (%maps%) do (
