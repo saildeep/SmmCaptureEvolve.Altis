ac89903e22f906b9032047b52fa7fe4a23ba5e50
saildeep <jkoerner@odd-it.com>
2017-06-16 02:45:23 +0200

    Added map build system to load maps from external file

--- build_maps.bat
+++ build_maps.bat
@@ -2,10 +2,10 @@
 SET maps=Tanoa Stratis Chernarus Takistan Australia lythium
 for %%a in (.) do set currentfolder=%%~na
 echo "%currentfolder%"
-(for %%a in (%maps%) do (
-   echo %%a
-REM cleanup old folders
-   del /S /F /Q "%CD%\..\%currentfolder%.%%a"
-   mkdir "%CD%\..\%currentfolder%.%%a"
-   xcopy /E "%CD%" "%CD%\..\%currentfolder%.%%a"
-))
+@echo off
+for /F "delims=" %%a in (maps.txt) do (
+     echo Generating %%a
+     del /S /F /Q "%CD%\..\%currentfolder%.%%a"
+     mkdir "%CD%\..\%currentfolder%.%%a"
+     xcopy /E "%CD%" "%CD%\..\%currentfolder%.%%a"
+)
