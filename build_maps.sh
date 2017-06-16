136891259ae5c049266dcc91283242098b251032
Jonas Körner <jkoerner@odd-it-main.yourvserver.net>
2017-06-16 03:01:18 +0200

    Add map build script for linux

---
+++ build_maps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+while read name
+do
+    name=$(echo $name| tr -d '\15\32')
+    rm -r $DIR/../$name
+    cp -r $DIR $DIR/../SmmCaptureEvolve.$name
+done < maps.txt
