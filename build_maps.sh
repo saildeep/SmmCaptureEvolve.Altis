5cf9102890b0110d2a7d356dabf75d22fb1b5db9
Arma 3 Server User <arma3@odd-it-main.yourvserver.net>
2017-06-16 13:34:21 +0200

    forced deletion of other folders in map build

--- build_maps.sh
+++ build_maps.sh
@@ -3,6 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 while read name
 do
     name=$(echo $name| tr -d '\15\32')
-    rm -r $DIR/../$name
+    rm -rf $DIR/../$name
     cp -r $DIR $DIR/../SmmCaptureEvolve.$name
 done < $DIR/maps.txt
