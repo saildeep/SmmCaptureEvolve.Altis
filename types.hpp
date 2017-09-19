23046543456c589ccdebeab2922b940395c41acd
Jonas Körner <jkoerner@odd-it.com>
2017-09-20 00:45:07 +0200

    Implemented zones as type

--- types.hpp
+++ types.hpp
@@ -1,4 +1,23 @@
 class Zone:OOType{
-    class Position:OOVar{};
-   
+    class Position:OOVar{
+        typeName = "ARRAY";
+        typeCheck = "(count _this) == 3";
+    };
+    class Hash:OOVar{
+        typeName="STRING";
+        typeCheck = "(count _this) > 0";
+    };
+    class Size:OOVar{
+        typeName="SCALAR";
+        typeCheck = "_this > 0";
+    };
+    class Owner:OOVar{
+        typeName = "SIDE";
+    };
+    class Neighbours:OOVar{
+        typeName = "ARRAY";
+    };
+    class DisplayName:OOVar{
+        typeName = "STRING";
+    };
 };
