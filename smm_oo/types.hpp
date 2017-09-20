09b7a0e6550d7f484c87f6037b17dbd8c57002d2
Jonas Körner <jkoerner@odd-it.com>
2017-09-20 18:11:18 +0200

    More refractoring

---
+++ smm_oo/types.hpp
@@ -0,0 +1,15 @@
+
+class EventDistributor:OOType{
+    class Name:OOVar{
+        typeName="STRING";
+    };
+
+    class EventListeners:OOVar{
+        typeName="[EventListener]";
+    };
+    class AddEventListener:OOFunction{
+        code = "([_this select 0] call EventDistributor_get_EventListeners) pushBack (_this select 1);";
+    };
+};
+
+class EventListener:OOType{};
--- types.hpp
+++ types.hpp
@@ -1,19 +1,5 @@
 
-class EventDistributor:OOType{
-    class Name:OOVar{
-        typeName="STRING";
-    };
-
-    class EventListeners:OOVar{
-        typeName="[EventListener]";
-    };
-    class AddEventListener:OOFunction{
-        code = "([_this select 0] call EventDistributor_get_EventListeners) pushBack (_this select 1);";
-    };
-};
-
-class EventListener:OOType{};
-
 
 #include "smm_config\types.hpp"
 #include "smm_spawner\types.hpp"
+#include "smm_oo\types.hpp"
