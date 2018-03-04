235a76e39c6f6f88003d64abc0502b1cd39886fd
Jonas Körner <jkoerner@odd-it.com>
2018-03-04 17:17:58 +0100

    Minor fix

--- mutex.hpp
+++ mutex.hpp
@@ -1,2 +1,2 @@
-#define MUTEX_LOCK(varname) waitUntil{(isNil{varname}) and {varname=true;true} };if(smm_debug)then{diag_log "Locking mutex varname";};
-#define MUTEX_UNLOCK(varname) if(isNil{varname})then{throw "Trying to unlock not locked mutex "};varname=nil;if(smm_debug)then{diag_log "Unlocked mutex varname";};
+#define MUTEX_LOCK(varname) waitUntil{(isNil{varname}) and {varname=true;true} };if(smm_debug)then{diag_log ("Locking mutex " + #varname) ;};
+#define MUTEX_UNLOCK(varname) if(isNil{varname})then{throw "Trying to unlock not locked mutex "};varname=nil;if(smm_debug)then{diag_log ("Unlocked mutex " + #varname ) ;};
