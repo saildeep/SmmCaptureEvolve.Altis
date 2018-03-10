d9827948364292b0b78a4872ab56aa6161e1da69
Jonas Körner <jkoerner@odd-it.com>
2018-03-11 00:18:51 +0100

    Fixed missing ; for ai param

--- params.hpp
+++ params.hpp
@@ -137,7 +137,7 @@
 
     class aiSubSkillAimingAccuracy
     {  
-        title="AI Sub Skill: Aiming Accuracy"
+        title="AI Sub Skill: Aiming Accuracy";
         texts[]={"Auto","20%","30%","40%","50%","60%","70%","80%","90%","100%"};
         values[]={0,2,3,4,5,6,7,8,9,10};
         default  = 0;
@@ -146,7 +146,7 @@
 
     class aiSubSkillAimingShake
     {  
-        title="AI Sub Skill: Aiming Shake"
+        title="AI Sub Skill: Aiming Shake";
         texts[]={"Auto","20%","30%","40%","50%","60%","70%","80%","90%","100%"};
         values[]={0,2,3,4,5,6,7,8,9,10};
         default  = 0;
@@ -155,7 +155,7 @@
 
     class aiSubSkillAimingSpeed
     {  
-        title="AI Sub Skill: Aiming Speed   -> Hint: interpolated between 0.5 and 1.0"
+        title="AI Sub Skill: Aiming Speed   -> Hint: interpolated between 0.5 and 1.0";
         texts[]={"Auto","20%","30%","40%","50%","60%","70%","80%","90%","100%"};
         values[]={0,2,3,4,5,6,7,8,9,10};
         default  = 0;
@@ -164,7 +164,7 @@
     
     class aiSubSkillSpotDistance
     {  
-        title="AI Sub Skill: SpotDistance"
+        title="AI Sub Skill: SpotDistance";
         texts[]={"Auto","20%","30%","40%","50%","60%","70%","80%","90%","100%"};
         values[]={0,2,3,4,5,6,7,8,9,10};
         default  = 0;
@@ -173,7 +173,7 @@
 
     class aiSubSkillSpotTime
     {  
-        title="AI Sub Skill: Spot Time      -> Hint: interpolated between 0.0 and 0.7"
+        title="AI Sub Skill: Spot Time      -> Hint: interpolated between 0.0 and 0.7";
         texts[]={"Auto","20%","30%","40%","50%","60%","70%","80%","90%","100%"};
         values[]={0,2,3,4,5,6,7,8,9,10};
         default  = 0;
@@ -182,7 +182,7 @@
 
     class aiSubSkillReloadSpeed
     {  
-        title="AI Sub Skill: ReloadSpeed"
+        title="AI Sub Skill: ReloadSpeed";
         texts[]={"Auto","20%","30%","40%","50%","60%","70%","80%","90%","100%"};
         values[]={0,2,3,4,5,6,7,8,9,10};
         default  = 0;
@@ -191,7 +191,7 @@
 
     class aiSubSkillCommanding
     {  
-        title="AI Sub Skill: Commanding"
+        title="AI Sub Skill: Commanding";
         texts[]={"Auto","20%","30%","40%","50%","60%","70%","80%","90%","100%"};
         values[]={0,2,3,4,5,6,7,8,9,10};
         default  = 0;
@@ -200,7 +200,7 @@
 
     class aiSubSkillGeneral
     {  
-        title="AI Sub Skill: General"
+        title="AI Sub Skill: General";
         texts[]={"Auto","20%","30%","40%","50%","60%","70%","80%","90%","100%"};
         values[]={0,2,3,4,5,6,7,8,9,10};
         default  = 0;
