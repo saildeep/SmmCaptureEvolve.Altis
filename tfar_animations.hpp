847e34b6bd5e7fc514724c7ec354718c4bebcc22
Jonas Körner <jkoerner@odd-it.com>
2020-02-29 17:29:36 +0100

    Added forcing of animations

---
+++ tfar_animations.hpp
@@ -0,0 +1,18 @@
+class KKA3_Config
+{
+    class TFAR_Animations
+    {
+        class Vest
+        {
+            Animation_01[] = {"V_PlateCarrier1_rgr","Example3"};
+            Animation_02[] = {"Example2"};
+            Animation_03[] = {"Example3"};
+        };     
+        class Helmet
+        {
+            Animation_01[] = {"Example1"};
+            Animation_02[] = {"Example2"};
+            Animation_03[] = {"Example3"};
+        };
+    };
+};
